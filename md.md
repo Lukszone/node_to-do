530545fc9b1a27f454595d7092068039e1e098fc
dev-cardenas <dev.cardenas@gmail.com>
2021-11-30 18:06:04 -0300

    mod

---
+++ md.md
@@ -0,0 +1,6 @@
+3m ancho
+2m alto
+
+circuito incluir, vectorizar mapa, ponerle circuito 35% del espacio
+
+logo, de colores vectores, el corredor, full color
--- routes/index.js
+++ routes/index.js
@@ -3,48 +3,45 @@ const path = require("path");
 const router = express.Router();
 import * as fs from "fs";
 import { v4 as uuid4 }  from "uuid";
-// const tareas = require("../public/js/script")
-module.exports = router;
 
-router.get("/", function (req, res) {
+
+router.get("/", async (req, res) => {
+  console.log("i am here")
   // res.sendFile(path.join(__dirname, "../views/index.hbs"));
   let tasks = fs.readFileSync("./task.json");
   tasks = JSON.parse(tasks);
+
   res.render('home', {
     tasks: tasks.tasks
   })
 
-  //res.sendFile("index.html");
-
-  //const task = tareas.find().lean();
-  //res.render("index", { tareas: task });
 });
+
+
 router.get(`/tarea/:id/actualizar`, (req, res) => {
 res.sendFile(path.join(__dirname, "../views/actualizar.hbs"));
 // let task =  
 // res.render("actualizar.html", { task });
 });
-//router.post("/tarea/:id/", (req, res) => {
-//res.sendFile(path.join(__dirname, "../views/actualizar.html"));
-// const { id } = req.params;
-// tareas.findByIdAndUpdate(id, req.body);
-// res.redirect("/");
-//});
+
 
 router.post("/tarea_nueva", async (req, res) => {
   try {
-    var tarea = req.body;
+    var tarea = req.body.tarea;
     let tasks = fs.readFileSync("./task.json");
     tasks = JSON.parse(tasks);
-    console.log(tarea, tasks)
 
-    tasks.tasks.push({ id: uuid4(), tarea });
+    tasks.tasks.push({ id: uuid4(), tarea: tarea });
 
     fs.writeFileSync("./task.json", JSON.stringify(tasks));
 
-    res.json({ message: "saved" });
+    res.render('home', {
+      tasks: tasks.tasks
+    })
   } catch (e) {
     console.log(e);
     res.json({ message: "error" });
   }
 });
+
+module.exports = router;
