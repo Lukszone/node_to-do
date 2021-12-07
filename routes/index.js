e6eb281baeddf04f2f902496f28c36d1f197a640
Lukszone <isaacamarilla252@gmail.com>
2021-12-07 15:57:39 -0300

    config backend routes to apdate and delete tasks

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,88 @@
 const express = require("express");
-const path = require("path");
 const router = express.Router();
 import * as fs from "fs";
-import { v4 as uuid4 }  from "uuid";
-
+import { v4 as uuid4 } from "uuid";
 
 router.get("/", async (req, res) => {
-  console.log("i am here")
-  // res.sendFile(path.join(__dirname, "../views/index.hbs"));
-  let tasks = fs.readFileSync("./task.json");
-  tasks = JSON.parse(tasks);
+  let tasks = await fs.readFileSync("./task.json");
+  tasks = await JSON.parse(tasks);
 
-  res.render('home', {
-    tasks: tasks.tasks
-  })
-
+  res.render("home", {
+    tasks: tasks.tasks,
+  });
 });
 
-
-router.get(`/tarea/:id/actualizar`, (req, res) => {
-res.sendFile(path.join(__dirname, "../views/actualizar.hbs"));
-// let task =  
-// res.render("actualizar.html", { task });
-});
-
-
-router.post("/tarea_nueva", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     var tarea = req.body.tarea;
     let tasks = fs.readFileSync("./task.json");
     tasks = JSON.parse(tasks);
 
-    tasks.tasks.push({ id: uuid4(), tarea: tarea });
+    await tasks.tasks.push({ id: uuid4(), tarea: tarea });
 
     fs.writeFileSync("./task.json", JSON.stringify(tasks));
 
-    res.render('home', {
-      tasks: tasks.tasks
-    })
+    res.render("home", {
+      tasks: tasks.tasks,
+    });
   } catch (e) {
     console.log(e);
     res.json({ message: "error" });
   }
 });
+
+router.get("/actualizar/:id", async (req, res) => {
+  let { id } = req.params;
+  let tasks = fs.readFileSync("./task.json");
+  tasks = await JSON.parse(tasks);
+
+  let [task] = tasks.tasks.filter((task) => task.id === id);
+  if (!task) {
+    res.redirect("/");
+  }
+
+  res.render("actualizar", {
+    task,
+  });
+});
+
+router.post("/actualizar/:id", async (req, res) => {
+  let { id } = req.params;
+
+  let tasks = fs.readFileSync("./task.json");
+  tasks = JSON.parse(tasks);
+  tasks = tasks.tasks.map((task) => {
+    if (task.id === id) {
+      return {
+        tarea: req.body.tarea,
+        id,
+      };
+    }
+
+    return task;
+  });
+
+  fs.writeFileSync("./task.json", JSON.stringify({ tasks }));
+
+  res.redirect("/");
+});
+
+router.get("/eliminar/:index", async (req, res) => {
+  let { index } = req.params;
+  index = parseInt(index);
+  let tasks = fs.readFileSync("./task.json");
+  tasks = JSON.parse(tasks);
+
+  let taskDelete = {
+    tasks:[
+    ...tasks.tasks.slice(0, index),
+    ...tasks.tasks.slice(index + 1)
+  ],
+  };
+
+  fs.writeFileSync("./task.json", JSON.stringify(taskDelete));
+  res.redirect("/");
+});
+
 
 module.exports = router;
