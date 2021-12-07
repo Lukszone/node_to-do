573fb7d08730abcfcb166a553c2e0b8e6b1f0eae
Lukszone <isaacamarilla252@gmail.com>
2021-12-07 15:38:01 -0300

    add ignore to nodemon, add morgan, config handlebars views and layouts

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import path from "path";
+import morgan from "morgan";
 
 const router = require("./routes");
 
 const app = express();
 
-// app.engine(
-app.engine('.hbs', 
+app.engine(
+  '.hbs', 
   engine({
     extname: '.hbs',
     defaultLayout: "main",
-    layoutsDir: path.join(__dirname, "views", "layout"),
-  }));
+    layoutsDir: 
+    path.join(__dirname, "views", "layout"),
+  })
+);
 app.set('view engine', '.hbs');
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded());
+app.use(morgan('dev'));
+app.use(express.static(path.join(
+  __dirname, 'public'
+  )
+));
+app.use(express.urlencoded({
+  extended:true
+}));
 app.use(express.json());
 app.use(router);
 
