af47e37ca19f4dbf0732eafcb404889a7353e009
Lukszone <isaacamarilla252@gmail.com>
2022-01-13 14:40:17 -0300

    Add style for begin, login and register

--- routes/index.js
+++ routes/index.js
@@ -7,31 +7,34 @@ import { TaskController } from "../controllers/TaskControllers";
 
 const router = express.Router();
 
-//bienvenida
+//Welcome
 router.get("/", async (req, res) => {
   res.render("begin");
 });
-//login
+//Login
 router.get("/login", async (req, res) => {
   res.render("login");
 });
 router.get("/logout", AuthController.logout);
 router.post("/login", AuthController.login);
-//registration
+//Registration
 router.get("/registro", async (req, res) => {
   res.render("registro");
 });
 router.post("/registro", RegisterController.register);
 
-// Home
+//Home
 router.get("/inicio", verifyToken, HomeController.home);
 router.post("/inicio", verifyToken, TaskController.home_post);
-// Update
+
+//Update
 router.get("/actualizar/:id", TaskController.actualizar);
 router.post("/actualizar/:id", TaskController.actualizar_post);
-//delete
+
+//Delete
 router.get("/eliminar/:index", TaskController.delete);
-//done
+
+//Done
 router.post("/terminada/:id/false", TaskController.done_false);
 router.post("/terminada/:id/true", TaskController.done_true);
 
